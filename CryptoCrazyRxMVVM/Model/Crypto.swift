9a0af237f6e0c181f2cd2b733bd4726513619683
Doğukan Küçükler <dogukankucuklerr@gmail.com>
2023-08-01 12:13:55 +0300

    Initial Commit

---
+++ CryptoCrazyRxMVVM/Model/Crypto.swift
@@ -0,0 +1,14 @@
+//
+//  Crypto.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by Doğukan Küçükler on 31.07.2023.
+//
+
+import Foundation
+
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
